4587409963cfefdbe1d3b76f9fece58dde112315
mother-art <doppel-w-8oso@ezweb.ne.jp>
2022-07-14 11:16:20 +0900

    ユーザー登録機能実装

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
